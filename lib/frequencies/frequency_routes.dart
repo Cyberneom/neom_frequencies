444a4216aee569ff4b8a3f3079099f7708bbdb09
emmanuel-montoya <emmanuel.montoyae@gmail.com>
2023-07-22 11:52:09 -0600

    First commit for root frequencies module

---
+++ lib/frequencies/frequency_routes.dart
@@ -0,0 +1,22 @@
+import 'package:get/get.dart';
+
+import 'package:neom_commons/core/utils/constants/app_route_constants.dart';
+import 'package:neom_frequencies/frequencies/ui/frequency_page.dart';
+import 'ui/root_frequencies_page.dart';
+
+class FrequencyRoutes {
+
+  static final List<GetPage<dynamic>> routes = [
+    GetPage(
+      name: AppRouteConstants.frequencyFav,
+      page: () => const RootFrequenciesPage(),
+      transition: Transition.leftToRight,
+    ),
+    GetPage(
+      name: AppRouteConstants.frequency,
+      page: () => const FrequencyPage(),
+      transition: Transition.rightToLeft,
+    ),
+  ];
+
+}
